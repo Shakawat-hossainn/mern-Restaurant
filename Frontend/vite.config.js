9e0dacbae8b1b1219b52486973fa700e67997d8e
Shakawat-hossainn <shakahossain044@gmail.com>
2024-06-23 12:58:16 +0600

    changes for deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mern-restaurant-backend-0xmm.onrender.com',
         secure:false
       },
     },
